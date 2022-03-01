33ba760bad6bb08ae789f945ccbf8f91134fdc45
jedmorris <jemorris110@gmail.com>
2022-03-01 14:18:30 -0800

    updated models

--- AtlasTracker/Models/ProjectPriority.cs
+++ AtlasTracker/Models/ProjectPriority.cs
@@ -1,6 +1,18 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
 namespace AtlasTracker.Models;
 
 public class ProjectPriority
 {
+    public int Id { get; set; }
+
+    [Required]
+    [DisplayName("Priority Name")]
+    public string? Name { get; set; }
+    
+    
+    
+    
     
 }
--- AtlasTracker/Models/TicketType.cs
+++ AtlasTracker/Models/TicketType.cs
@@ -1,6 +1,12 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
 namespace AtlasTracker.Models;
 
 public class TicketType
 {
     
+    public int Id { get; set; }
+
+    [Required] [DisplayName("Type Name")] public string? Name { get; set; }
 }
