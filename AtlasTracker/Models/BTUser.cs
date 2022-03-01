b13b3d011fad4a9e2563a970b51079aedcd5d9bf
jedmorris <jemorris110@gmail.com>
2022-03-01 08:43:17 -0800

    created BTUser Model that extends IdentityUser

---
+++ AtlasTracker/Models/BTUser.cs
@@ -0,0 +1,36 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+
+namespace AtlasTracker.Models;
+
+public class BTUser : IdentityUser
+{
+   [Required]
+   [StringLength(25, ErrorMessage = "The {0} must be at least {2} and at most {1} characters long.", MinimumLength = 2)]
+   [DisplayName("First Name")]
+   public string? FirstName { get; set; }
+   
+   [Required]
+   [StringLength(25, ErrorMessage = "The {0} must be at least {2} and at most {1} characters long.", MinimumLength = 2)]
+   [DisplayName("Last Name")]
+   public string? LastName { get; set; }
+   
+   [NotMapped]
+   [DisplayName("Full Name")]
+   public string? FullName { get { return $"{FirstName} {LastName}"; } }
+  
+   // Image handling
+   [NotMapped]
+   [DataType(DataType.Upload)]
+   public IFormFile? AvatarFormFil { get; set; }
+   
+   [DisplayName("Avatar")]
+   public string? AvatarName { get; set; }
+   
+   public byte[]? AvatarData { get; set; }
+   
+   [Display(Name = "File Extension")]
+   public string? AvatarContentType { get; set; }
+}
