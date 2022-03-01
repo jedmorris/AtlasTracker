058df71e50af7ede4081c4b6c9451b549f88ab5c
jedmorris <jemorris110@gmail.com>
2022-03-01 08:58:10 -0800

    added BTUser as the default Identity Service

--- AtlasTracker/Program.cs
+++ AtlasTracker/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using AtlasTracker.Data;
+using AtlasTracker.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,11 +10,9 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<BTUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
