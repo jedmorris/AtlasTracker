94ec566e3c32f6878c69655432c7370b9c62b4ed
jedmorris <jemorris110@gmail.com>
2022-03-01 14:07:22 -0800

    updated TicketPriority Model. Deleted SQL Migrations folder

--- AtlasTracker/Data/ApplicationDbContext.cs
+++ AtlasTracker/Data/ApplicationDbContext.cs
@@ -10,4 +10,18 @@ public class ApplicationDbContext : IdentityDbContext<BTUser>
         : base(options)
     {
     }
+
+    public DbSet<Company> Companies { get; set; } = default!;
+    public DbSet<Invite> Invites { get; set; } = default!;
+    public DbSet<Project> Projects { get; set; } = default!;
+    public DbSet<Ticket> Tickets { get; set; } = default!;
+    public DbSet<Notification> Notifications { get; set; } = default!;
+    public DbSet<NotificationType> NotificationTypes { get; set; } = default!;
+    public DbSet<ProjectPriority> ProjectPriorities { get; set; } = default!;
+    public DbSet<TicketAttachment> TicketAttachments { get; set; } = default!;
+    public DbSet<TicketComment> TicketComments { get; set; } = default!;
+    public DbSet<TicketHistory> TicketHistories { get; set; } = default!;
+    public DbSet<TicketPriority> TicketPriorities { get; set; } = default!;
+    public DbSet<TicketStatus> TicketStatuses { get; set; } = default!;
+    public DbSet<TicketType> TicketTypes { get; set; } = default!;
 }
--- AtlasTracker/Models/TicketPriority.cs
+++ AtlasTracker/Models/TicketPriority.cs
@@ -1,6 +1,11 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
 namespace AtlasTracker.Models;
 
 public class TicketPriority
 {
-    
+    public int Id { get; set; }
+
+    [Required] [DisplayName("Type Name")] public string? Name { get; set; }
 }
