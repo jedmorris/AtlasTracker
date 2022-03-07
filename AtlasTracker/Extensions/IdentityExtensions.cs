ba02e42177992d3b127c9349f3a1ef1aeeb10c25
jedmorris <jemorris110@gmail.com>
2022-03-07 08:59:46 -0800

    completed IdentityExtension

---
+++ AtlasTracker/Extensions/IdentityExtensions.cs
@@ -0,0 +1,13 @@
+using System.Security.Claims;
+using System.Security.Principal;
+
+namespace AtlasTracker.Extensions;
+
+public static class IdentityExtensions
+{
+   public static int GetCompanyId(this IIdentity identity)
+   {
+      Claim claim = ((ClaimsIdentity) identity).FindFirst("CompanyId")!;
+      return int.Parse(claim.Value);
+   }
+}
