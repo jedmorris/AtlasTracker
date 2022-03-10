4ab815132b9a1bc0ae28fd872a4a4e06aaa109ff
jedmorris <jemorris110@gmail.com>
2022-03-10 11:31:28 -0800

    scaffolded the following controllers with views: Notifications, NotificationTypes, ProjectPriorities, TicketAttachment, TicketComments, TicketHistory, TicketPriorities, TicketStatus, TicketType

---
+++ AtlasTracker/Controllers/TicketHistoryController.cs
@@ -0,0 +1,167 @@
+#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AtlasTracker.Data;
+using AtlasTracker.Models;
+
+namespace AtlasTracker.Controllers
+{
+    public class TicketHistoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TicketHistoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: TicketHistory
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.TicketHistories.Include(t => t.Ticket).Include(t => t.User);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: TicketHistory/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ticketHistory = await _context.TicketHistories
+                .Include(t => t.Ticket)
+                .Include(t => t.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (ticketHistory == null)
+            {
+                return NotFound();
+            }
+
+            return View(ticketHistory);
+        }
+
+        // GET: TicketHistory/Create
+        public IActionResult Create()
+        {
+            ViewData["TicketId"] = new SelectList(_context.Tickets, "Id", "Description");
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id");
+            return View();
+        }
+
+        // POST: TicketHistory/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,PropertyName,Description,Created,OldValue,NewValue,TicketId,UserId")] TicketHistory ticketHistory)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(ticketHistory);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["TicketId"] = new SelectList(_context.Tickets, "Id", "Description", ticketHistory.TicketId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", ticketHistory.UserId);
+            return View(ticketHistory);
+        }
+
+        // GET: TicketHistory/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ticketHistory = await _context.TicketHistories.FindAsync(id);
+            if (ticketHistory == null)
+            {
+                return NotFound();
+            }
+            ViewData["TicketId"] = new SelectList(_context.Tickets, "Id", "Description", ticketHistory.TicketId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", ticketHistory.UserId);
+            return View(ticketHistory);
+        }
+
+        // POST: TicketHistory/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,PropertyName,Description,Created,OldValue,NewValue,TicketId,UserId")] TicketHistory ticketHistory)
+        {
+            if (id != ticketHistory.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(ticketHistory);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TicketHistoryExists(ticketHistory.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["TicketId"] = new SelectList(_context.Tickets, "Id", "Description", ticketHistory.TicketId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", ticketHistory.UserId);
+            return View(ticketHistory);
+        }
+
+        // GET: TicketHistory/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ticketHistory = await _context.TicketHistories
+                .Include(t => t.Ticket)
+                .Include(t => t.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (ticketHistory == null)
+            {
+                return NotFound();
+            }
+
+            return View(ticketHistory);
+        }
+
+        // POST: TicketHistory/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var ticketHistory = await _context.TicketHistories.FindAsync(id);
+            _context.TicketHistories.Remove(ticketHistory);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TicketHistoryExists(int id)
+        {
+            return _context.TicketHistories.Any(e => e.Id == id);
+        }
+    }
+}
