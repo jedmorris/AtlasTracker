9e92b948981e52b3bad8836ae961075377f3a2f7
jedmorris <jemorris110@gmail.com>
2022-03-10 07:05:20 -0800

    updated format & hidden fields

--- AtlasTracker/Controllers/UserRolesController.cs
+++ AtlasTracker/Controllers/UserRolesController.cs
@@ -31,9 +31,9 @@ public class UserRolesController : Controller
     public async Task<IActionResult> ManageUserRoles()
     {
         // 1. Add an instance of the ViewModel as a list (model)
-        List<ManageUserRolesViewModel> model = new ();
-        
-        
+        List<ManageUserRolesViewModel> model = new();
+
+
         // 2. Get CompanyId
         int companyId = User.Identity!.GetCompanyId();
 
@@ -48,33 +48,43 @@ public class UserRolesController : Controller
             ManageUserRolesViewModel viewModel = new();
             viewModel.BtUser = user;
             IEnumerable<string> selected = await _rolesService.GetUserRolesAsync(user);
-            viewModel.Roles = new MultiSelectList(await _rolesService.GetRolesAsync(), "Name","Name", selected);
+            viewModel.Roles = new MultiSelectList(await _rolesService.GetRolesAsync(), "Name", "Name", selected);
 
             model.Add(viewModel);
         }
-           // 4. Return model to the View 
+
+        // 4. Return model to the View 
         return View(model);
     }
 
-    
+
     // POST
     [HttpPost]
     [ValidateAntiForgeryToken]
     public async Task<IActionResult> ManageUserRoles(ManageUserRolesViewModel member)
     {
+        // Get the company Id
+        int companyId = User.Identity.GetCompanyId();
+
+        // Instantiate the BTUser
+        BTUser btUser = (await _companyInfoService.GetAllMembersAsync(companyId)).FirstOrDefault(u => u.Id == member.BtUser?.Id);
+        
+        // Get roles for the user
+        IEnumerable<string> roles = await _rolesService.GetUserRolesAsync(btUser!);
+        
+        // Get Selected Roles for the User
+        string userRole = member.SelectedRoles?.FirstOrDefault()!;
+
+        if (!string.IsNullOrEmpty(userRole))
+        {
+            // Remove user from their roles
+            if (await _rolesService.RemoveUserFromRolesAsync(btUser!, roles))
+            {
+                // Add user to a new role
+                await _rolesService.AddUserToRoleAsync(btUser!, userRole);
+            }
+        }
+
         return RedirectToAction(nameof(ManageUserRoles));
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
