e46fe1f7901dca9ac02a32b00c40ead8c4a9116e
jedmorris <jemorris110@gmail.com>
2022-03-16 14:53:21 -0700

    cleared IBTEmailService methods

--- AtlasTracker/Services/Interfaces/IBTEmailService.cs
+++ AtlasTracker/Services/Interfaces/IBTEmailService.cs
@@ -2,12 +2,5 @@ namespace AtlasTracker.Services.Interfaces;
 
 public interface IBTEmailService
 {
-    public class MailSettings
-    {
-        public string? Mail { get; set; }
-        public string? DisplayName { get; set; }
-        public string? Password { get; set; }
-        public string? Host { get; set; }
-        public int Port { get; set; }
-    }
+
 }
