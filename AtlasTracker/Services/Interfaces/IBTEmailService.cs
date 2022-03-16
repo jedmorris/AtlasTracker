2ca46ba70474e88cfa276b948a1f79077af977dc
jedmorris <jemorris110@gmail.com>
2022-03-16 14:50:31 -0700

    updated Ticket/Details view and IBTEmailService 'MailSettings'

--- AtlasTracker/Services/Interfaces/IBTEmailService.cs
+++ AtlasTracker/Services/Interfaces/IBTEmailService.cs
@@ -2,5 +2,12 @@ namespace AtlasTracker.Services.Interfaces;
 
 public interface IBTEmailService
 {
-    
+    public class MailSettings
+    {
+        public string? Mail { get; set; }
+        public string? DisplayName { get; set; }
+        public string? Password { get; set; }
+        public string? Host { get; set; }
+        public int Port { get; set; }
+    }
 }
